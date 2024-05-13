1166b2d51fc251cf86cc5629c9362d9a9b5df1c1
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-05-13 07:35:21 +0600

    added ScrollRestoration component from react router dom to the MainLayout to scroll page to top on page change

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,17 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 
 const MainLayout = () => {
   return (
     <div>
+      <ScrollRestoration></ScrollRestoration>
+
       {/* Navbar */}
       <div className="shadow-md bg-accent">
         <Navbar></Navbar>
       </div>
+
       <div className="min-h-[calc(100vh-490px)]">
         <Outlet></Outlet>
       </div>
+
       <Footer></Footer>
     </div>
   );
