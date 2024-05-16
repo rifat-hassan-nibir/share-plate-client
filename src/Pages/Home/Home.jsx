f1e268230842e3582e3e8fe806515f8cafa0a7d6
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-05-16 16:50:56 +0600

    how it works section in home page complete

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,36 +1,11 @@
 import Hero from "./Hero";
-import FeaturedFoodCard from "./FeaturedFoodCard";
-import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
 import Gap from "../../Components/Gap";
-import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import { motion } from "framer-motion";
+import FeaturedFoods from "./FeaturedFoods";
+import HowItWorks from "./HowItWorks";
 
 const Home = () => {
-  // Get the food data using axios
-  const getFoodData = async () => {
-    try {
-      const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/foods?status=Available&quantity_sort=Descending&limit=6`);
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // Tanstack Query
-  const { data: featuredFoods = [], isError, error } = useQuery({ queryKey: ["featuredFoods"], queryFn: () => getFoodData() });
-
-  // Show error
-  if (isError) {
-    console.log(isError, error);
-    return (
-      <div className="flex justify-center items-center min-h-[calc(100vh-490px)]">
-        <p className="text-center text-3xl font-bold">Something Went Wrong! Please Reload.</p>
-      </div>
-    );
-  }
-
   return (
     <div>
       {/* Tab title */}
@@ -48,37 +23,9 @@ const Home = () => {
 
       <Gap></Gap>
 
-      {/* Show Featured Foods */}
-      <motion.div
-        initial={{ scale: 0.5, opacity: 0 }}
-        whileInView={{ scale: 1, opacity: 1 }}
-        transition={{ type: "twin", stiffness: 100, duration: 0.5 }}
-        className="px-4 lg:px-0 lg:py-[100px] py-[50px] bg-accent"
-      >
-        <h1 className="text-center font-bold text-[22px] lg:text-[36px] lg:mb-[50px] mb-[25px]">Featured Foods</h1>
-        {/* Show loader when data is still loading */}
-        {featuredFoods.length === 0 && (
-          <div className="text-center mt-[100px]">
-            <span className="loading loading-spinner loading-lg "></span>
-          </div>
-        )}
+      <HowItWorks></HowItWorks>
 
-        <div className="container mx-auto grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-[32px]">
-          {featuredFoods.map((food, index) => (
-            <FeaturedFoodCard food={food} key={index}></FeaturedFoodCard>
-          ))}
-        </div>
-
-        <div className="flex justify-center">
-          <Link
-            to="/available-foods"
-            type="button"
-            className="py-3 px-6 mt-[40px] lg:mt-[80px] inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-primary text-white transition-all hover:bg-secondary hover:text-black disabled:opacity-50 disabled:pointer-events-none"
-          >
-            <button>Show All Avaiable Foods</button>
-          </Link>
-        </div>
-      </motion.div>
+      <FeaturedFoods></FeaturedFoods>
     </div>
   );
 };
---
+++ src/Pages/Home/HowItWorks.jsx
@@ -0,0 +1,114 @@
+import sharingFood from "../../assets/sharing-food.jpg";
+
+const HowItWorks = () => {
+  return (
+    <div className="bg-accent py-[50px] lg:py-[100px]">
+      <div className="container px-4 lg:px-0 mx-auto">
+        {/* Grid  */}
+        <div className="md:grid md:grid-cols-2 md:items-center md:gap-12 xl:gap-32">
+          <div className="mt-5 sm:mt-10 lg:mt-0">
+            <div className="space-y-6 sm:space-y-8">
+              {/* Title  */}
+              <div className="space-y-2 md:space-y-4">
+                <h2 className="font-bold text-3xl lg:text-4xl text-gray-800 dark:text-neutral-200">How it works?</h2>
+                <p className="text-gray-500 dark:text-neutral-500">
+                  Welcome to Share Plate! We&apos;re a community-driven platform dedicated to bringing people together through the love of
+                  food. Our mission is to connect home cooks, food enthusiasts, and local farmers to share recipes, tips, and culinary
+                  experiences. Whether you&apos;re looking to discover new dishes or share your own culinary creations, Share Plate is the
+                  place to celebrate the joy of cooking and eating together. Join us in building a vibrant community where every plate tells
+                  a story!
+                </p>
+              </div>
+              {/* End Title  */}
+
+              {/* List  */}
+              <ul className="space-y-2 sm:space-y-4">
+                <li className="flex space-x-3">
+                  <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+                    <svg
+                      className="flex-shrink-0 size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <polyline points="20 6 9 17 4 12" />
+                    </svg>
+                  </span>
+
+                  <span className="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
+                    <span className="font-bold">Join Our Community:</span> Sign up for a free account to become a member of Share Plate.
+                    Whether you&apos;re a home cook, food enthusiast, or local farmer, everyone is welcome!
+                  </span>
+                </li>
+
+                <li className="flex space-x-3">
+                  <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+                    <svg
+                      className="flex-shrink-0 size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <polyline points="20 6 9 17 4 12" />
+                    </svg>
+                  </span>
+
+                  <span className="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
+                    <span className="font-bold">Add Your Food:</span> Fillup the form in the Add Food Page to list your food.
+                  </span>
+                </li>
+
+                <li className="flex space-x-3">
+                  <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+                    <svg
+                      className="flex-shrink-0 size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <polyline points="20 6 9 17 4 12" />
+                    </svg>
+                  </span>
+
+                  <span className="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
+                    <span className="font-bold">Wait For a Request:</span>People will request for your food and contact you through your
+                    given email.
+                  </span>
+                </li>
+              </ul>
+              {/* End List  */}
+            </div>
+          </div>
+          {/* End Col  */}
+
+          {/* Col Start */}
+          <div>
+            <img className="rounded-xl" src={sharingFood} alt="Image Description" />
+          </div>
+          {/* Col End */}
+        </div>
+        {/* End Grid  */}
+      </div>
+    </div>
+  );
+};
+
+export default HowItWorks;
